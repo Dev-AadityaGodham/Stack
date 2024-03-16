427d0008f0e227282a1bec1820cc420067e1fae6
Dev-AadityaGodham <159020029+Dev-AadityaGodham@users.noreply.github.com>
2024-03-16 20:41:04 +0530

    Create ForEach.java

---
+++ ForEach.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4};
+		for(int val : arr){
+		    if(val==2){
+		        System.out.println("value 2");
+		    }
+		    System.out.println(val);
+		}
+	}
+}
